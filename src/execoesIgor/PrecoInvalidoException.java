291e8db60e44bb26b912ccd1e582f4059ee41b00
Igor Morais <igorlimamorais12@gmail.com>
2025-08-21 22:15:55 -0300

    Update PrecoInvalidoException.java

--- src/execoesIgor/PrecoInvalidoException.java
+++ src/execoesIgor/PrecoInvalidoException.java
@@ -1,5 +1,9 @@
 package execoes;
 
-public class PrecoInvalidoException {
+public class PrecoInvalidoException extends Exception {
+
+	public PrecoInvalidoException(String mensagem) {
+		super(mensagem);
+	}
 
 }
